b7843567c102c4112a820b7a4e93726ba569288f
devarias <gedafu2005@gmail.com>
2021-12-09 21:29:20 -0500

    0. Lockboxes

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def canUnlockAll(boxes):
+    keys = set()
+    for next_key, box in enumerate(boxes):
+        next_key += 1
+        for key in box:
+            if key < len(boxes):
+                keys.add(key)
+                keys.update(boxes[key])
+            if len(keys) == len(boxes) or len(keys) == len(boxes) - 1:
+                return True
+        if next_key not in keys:
+            return False
