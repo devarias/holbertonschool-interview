609a3adf0eac222c98b796f5ca77b259e2054d56
David Arias <gedafu2005@gmail.com>
2022-01-29 09:32:04 -0500

    0. Minimum Operations

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 import math
-"""module comment"""
+"""documentation"""
 
 
 def minOperations(n):
-    """ a method that calculates the fewest number of
-        operations needed to result in exactly n H characters in the file.
-    """
+    """documentation"""
     res = 0
     if n > 1:
         for i in range(2, int(math.sqrt(n) + 1)):
