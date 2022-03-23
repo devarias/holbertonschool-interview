f23bc68f46ece6ebfcd876880e05a8799b91fb6d
David Arias <gedafu2005@gmail.com>
2022-03-23 03:43:59 -0500

    0. AVL - From sorted array

--- 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
+++ 0x0D-sorted_array_to_avl/0-sorted_array_to_avl.c
@@ -8,7 +8,7 @@
  */
 avl_t *sorted_array_to_avl(int *array, size_t size)
 {
-    return !array ? NULL : insert_AVL_node(NULL, array, 0, (int)size - 1);
+	return !array ? NULL : insert_AVL_node(NULL, array, 0, (int)size - 1);
 }
 
 /**
@@ -21,18 +21,18 @@ avl_t *sorted_array_to_avl(int *array, size_t size)
  */
 avl_t *insert_AVL_node(avl_t *parent, int *array, int start, int end)
 {
-    size_t mid;
-    avl_t *root = NULL;
+	size_t mid;
+	avl_t *root = NULL;
 
-    if (start > end)
-        return (NULL);
-    mid = (start + end) / 2;
-    root = new_node(parent, array[mid]);
-    if (!root)
-        return (NULL);
-    root->left = insert_AVL_node(root, array, start, mid - 1);
-    root->right = insert_AVL_node(root, array, mid + 1, end);
-    return (root);
+	if (start > end)
+		return (NULL);
+	mid = (start + end) / 2;
+	root = new_node(parent, array[mid]);
+	if (!root)
+		return (NULL);
+	root->left = insert_AVL_node(root, array, start, mid - 1);
+	root->right = insert_AVL_node(root, array, mid + 1, end);
+	return (root);
 }
 
 /**
@@ -43,13 +43,13 @@ avl_t *insert_AVL_node(avl_t *parent, int *array, int start, int end)
  */
 avl_t *new_node(avl_t *parent, size_t value)
 {
-    avl_t *node = malloc(sizeof(avl_t));
+	avl_t *node = malloc(sizeof(avl_t));
 
-    if (!node)
-        return (NULL);
-    node->n = value;
-    node->parent = parent;
-    node->left = NULL;
-    node->right = NULL;
-    return (node);
+	if (!node)
+		return (NULL);
+	node->n = value;
+	node->parent = parent;
+	node->left = NULL;
+	node->right = NULL;
+	return (node);
 }
