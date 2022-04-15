c1f91f429104712e5bafa65e028ff2dc86f37d73
David Arias <gedafu2005@gmail.com>
2022-04-15 08:00:34 -0500

    0. Star Wars Characters

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,32 +1,31 @@
 #!/usr/bin/node
-
 const request = require('request');
 
-async function sw (id) {
-  const url = `https://swapi-api.hbtn.io/api/films/${id}`;
+async function sw(id) {
+	const url = `https://swapi-api.hbtn.io/api/films/${id}`;
 
-  request(url, async function (err, response, body) {
-    if (err) {
-      console.log(err);
-    } else {
-      for (const ch of JSON.parse(body).characters) {
-        const ret = () => {
-          return new Promise((resolve, reject) => {
-            request(ch, function (err, response, body) {
-              if (err) {
-                console.log(err);
-              } else {
-                resolve(JSON.parse(body).name);
-              }
-            });
-          });
-        };
-        console.log(await ret());
-      }
-    }
-  });
+	request(url, async (err, res, body) => {
+		if (err) {
+			console.log(err);
+		} else {
+			for (const c of JSON.parse(body).characters) {
+				const sol = () => {
+					return new Promise((resolve, reject) => {
+						request(c, (err, res, body) => {
+							if (err) {
+								console.log(err);
+							} else {
+								resolve(JSON.parse(body).name);
+							}
+						});
+					});
+				};
+				console.log(await sol());
+			}
+		}
+	});
 }
 
 if (process.argv.length === 3) {
-  sw(process.argv[2]);
+	sw(process.argv[2]);
 }
